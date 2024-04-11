7155c2fbcd3cbfaffd421714d9ce7a772f37336c
DinhHongKong <83532836+dinhhongkong@users.noreply.github.com>
2024-04-11 09:44:49 +0700

    update gateway

--- ServiceLogin/src/main/java/com/example/servicelogin/controller/HomeController.java
+++ ServiceLogin/src/main/java/com/example/servicelogin/controller/HomeController.java
@@ -1,13 +1,12 @@
-package com.example.servicequanly.controller;
+package com.example.servicelogin.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class QuanLyController {
-    @GetMapping("/quanly/home")
+public class HomeController {
+    @GetMapping("/")
     public String home() {
-        return "home quan ly";
+        return "home";
     }
-
 }
